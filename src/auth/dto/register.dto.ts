91c3fb58c0bd27decce93556f331014e268827d5
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2025-03-06 16:00:08 +0300

    prisma schema

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsEmail()
+  @IsString()
+  email: string;
+  @MinLength(6)
+  password: string;
+}
